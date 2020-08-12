c9c8ab5c0cf9c670b01f5cfb38a2cb79faf4fe31
yuda1124 <yuda1124@naver.com>
2020-08-13 01:26:59 +0900

    [#4] feat: 추가 - window 사이즈를 계산하는 react hook 추가
    - window width에 따라 slide 수를 다르게 하기 위해 hook 추가
    - 750px 이하는 1개 이상은 4개가 보이도록 옵션 수정

--- client/src/components/Swiper/Swiper.tsx
+++ client/src/components/Swiper/Swiper.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+// Import Swiper React components
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { StyledInput } from './Swiper.styles';
+// Import Swiper styles
 import 'swiper/swiper.scss';
 
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
+
 export default function ADSwiper() {
+  const { height, width } = useWindowDimensions();
+  console.log(width);
   return (
     <Swiper
       spaceBetween={50}
-      slidesPerView={1}
+      slidesPerView={width < 750 ? 1 : 4}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper: any) => console.log(swiper)}
     >
