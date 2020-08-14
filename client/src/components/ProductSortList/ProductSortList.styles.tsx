c00b3abd49b6ae87284309eb87a1812e835c4a37
sooojungee <sooojungee@gmail.com>
2020-08-14 13:42:15 +0900

    [#12] feat:추가 - ProductSortList
    product들이 정렬되어 보여지는 리스트 마크업 작성

---
+++ client/src/components/ProductSortList/ProductSortList.styles.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { Sort } from '@material-ui/icons';
+import { Grid } from '@material-ui/core';
+
+export const StyledListTitle = styled.div`
+  padding: 6px 0;
+  font-size: 15px;
+  text-align: right;
+  width: 100%;
+`;
+
+export const StyledListWrapper = styled.div`
+  background: white;
+  padding: 8px 0;
+  position: relative;
+`;
+
+export const StyledSortList = styled(Grid)`
+  width: 100%;
+`;
+
+export const SortIcon = styled(Sort)`
+  font-size: 14px !important;
+`;
