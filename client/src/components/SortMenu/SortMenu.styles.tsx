3c592fb839d04dd90344563cce08d3877a7555dd
sooojungee <sooojungee@gmail.com>
2020-08-15 21:39:16 +0900

    [#42] feat:추가-SortMenu
    SortMenu 컴포넌트 구조 추가

---
+++ client/src/components/SortMenu/SortMenu.styles.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledSortMenu = styled.div`
+  width: 100%;
+  border-radius: 20%;
+  height: 300px;
+  background: green;
+`;
---
+++ client/src/components/SortMenu/SortMenu.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { StyledSortMenu } from './SortMenu.styles';
+
+const SortMenuModal = () => {
+  return <StyledSortMenu>asdf</StyledSortMenu>;
+};
+
+export default SortMenuModal;
