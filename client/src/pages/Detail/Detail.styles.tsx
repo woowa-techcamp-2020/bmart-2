9cdc15d549176b133f1a0b589eff9e33be51a846
sooojungee <sooojungee@gmail.com>
2020-08-19 01:08:13 +0900

    [#48] feat:추가- Detail Page
    detali page의 전체적인 스타일 적용
    더미 데이터를 사용하여 페이지 구성하였습니다.
    selectList로 데이터와 set함수를 넘겨줍니다.
    
    모바일 뷰 위주로 구현했습니다

--- client/src/pages/Detail/Detail.styles.tsx
+++ client/src/pages/Detail/Detail.styles.tsx
@@ -1,3 +1,67 @@
 import styled from 'styled-components';
 
-export default {};
+interface DetailPageType {
+  src?: string;
+}
+
+export const StyledDetialWrapper = styled.div`
+  width: 100%;
+  min-height: calc(100vh - 60px);
+`;
+
+export const StyledImage = styled.div`
+  width: 100%;
+  padding: 16px;
+  margin: 16px 0;
+  height: 600px;
+  @media (max-width: 600px) {
+    height: 380px;
+  }
+  border-radius: 12px;
+  background-image: url('${(props: DetailPageType) => props.src}');
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+export const StyledCartButton = styled.div`
+  position: fixed;
+  width: calc(100% - 32px);
+  border-radius: 16px;
+  background: #dd4470;
+  bottom: 24px;
+  height: 48px;
+  color: white;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 48px;
+  text-align: center;
+`;
+
+export const StyledNameText = styled.div`
+  width: 100%;
+  font-size: 18px;
+  margin-bottom: 8px;
+`;
+
+export const StyledPriceText = styled.div`
+  width: 100%;
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 4px;
+`;
+
+export const InfoContent = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 4px 0;
+  line-height: 16px;
+  div {
+    line-height: 16px;
+  }
+`;
+
+export const InfoTitle = styled.div`
+  width: 66px;
+  font-weight: 600;
+`;
--- client/src/pages/Detail/Detail.tsx
+++ client/src/pages/Detail/Detail.tsx
@@ -1,7 +1,54 @@
 import * as React from 'react';
+import {
+  StyledDetialWrapper,
+  StyledCartButton,
+  StyledImage,
+  StyledNameText,
+  StyledPriceText,
+  InfoContent,
+  InfoTitle,
+} from './Detail.styles';
+
+import SelectList from '../../components/SelectList';
+
+const data = {
+  price: 3400,
+  max_quantity: 10,
+  name: '[다담] 찌개 양념장 6종',
+  discount: 0,
+  subCategory: '서브카테고리',
+  stock: 100,
+  imgUrl: 'https://img-cf.kurly.com/shop/data/goods/1530775904381y0.jpg',
+};
+
+// TODO
+// Util > 가격 string으로 변환 후 보여줘야함
+// 상세 페이지 내용 추가되어야 함
 
 const Detail = () => {
-  return <h1>Detail</h1>;
+  const [openList, setOpenList] = React.useState(false);
+
+  const onClick = React.useCallback(() => {
+    setOpenList(true);
+  }, [openList]);
+
+  return (
+    <StyledDetialWrapper>
+      <StyledImage className="image" src={data.imgUrl} />
+      <StyledNameText>{data.name}</StyledNameText>
+      <StyledPriceText>{data.price}원</StyledPriceText>
+      <InfoContent>
+        <InfoTitle>배달 정보</InfoTitle>
+        <div> 😢 지금은 운영 시간이 아니에요</div>
+      </InfoContent>
+      <StyledCartButton onPointerUp={onClick}>담기</StyledCartButton>
+      <SelectList
+        openList={openList}
+        setOpenList={setOpenList}
+        data={data}
+      ></SelectList>
+    </StyledDetialWrapper>
+  );
 };
 
 export default Detail;
