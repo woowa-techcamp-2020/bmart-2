428b4c0bd9da3b9c650b5ad71e1bef14d482ffb7
yuda1124 <yuda1124@naver.com>
2020-08-20 13:35:59 +0900

    [#5] feat: 구현 - 무한 swipe 구현
    - test data default props로 설정 (후에 삭제 예정)
    - swipe index가 0이거나, length + 1일때 scroll 위치 바꿔주는 기능 추가
    - scroll-snap-type 설정으로 최종 scroll 위치에서 가장 가까운 element로 scroll 되는 설정 추가

--- client/src/components/Carousel/Carousel.styles.tsx
+++ client/src/components/Carousel/Carousel.styles.tsx
@@ -2,25 +2,28 @@ import styled from 'styled-components';
 
 const StyledWrapper = styled.div`
   width: 100%;
-  background-color: blue;
   position: relative;
   overflow: hidden;
-  ::-webkit-scrollbar {
-    height: 0px; /* Remove scrollbar space */
-  }
 `;
 
 const StyledCarousel = styled.div`
   display: flex;
   overflow-x: auto;
+  scroll-snap-type: x mandatory;
+  ::-webkit-scrollbar {
+    height: 0px; /* Remove scrollbar space */
+  }
 `;
 
 const StyledHourse = styled.div`
   width: 80%;
-  padding-right: 5%;
+  margin-left: -5%;
+  padding-left: 10%;
   display: inline-block;
   border-radius: 15px;
   flex: 0 0 auto;
+  scroll-snap-align: start;
+  scroll-snap-stop: always;
 `;
 
 const StyledThumb = styled.img`
--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
 import {
   StyledWrapper,
   StyledHourse,
@@ -6,70 +7,141 @@ import {
   StyledThumb,
 } from './Carousel.styles';
 
-const bannerImg = [
-  {
-    id: 0,
-    img_url:
-      'https://img-cf.kurly.com/shop/data/m/event/fa9d6731b3f39c03dc830c02c414cc52.jpg',
-  },
-  {
-    id: 1,
-    img_url:
-      'https://img-cf.kurly.com/shop/data/m/event/bc9b91586a619ffcfa8d7d5de83aae86.jpg',
-  },
-  {
-    id: 2,
-    img_url:
-      'https://img-cf.kurly.com/shop/data/m/event/758688e5267c724d33ebcab68de2599c.jpg',
-  },
-  {
-    id: 3,
-    img_url:
-      'https://img-cf.kurly.com/shop/data/m/event/fa9d6731b3f39c03dc830c02c414cc52.jpg',
-  },
-  {
-    id: 1,
-    img_url:
-      'https://img-cf.kurly.com/shop/data/m/event/bc9b91586a619ffcfa8d7d5de83aae86.jpg',
-  },
-];
+interface bannerInfo {
+  id: number;
+  img_url: string;
+}
 
-export default function Carousel() {
+interface CarouselProps {
+  banners: Array<bannerInfo>;
+}
+
+export default function Carousel({ banners }: CarouselProps) {
   const carouselRef = useRef<HTMLDivElement>(null);
+  let swiperInterval: any = null;
 
   useEffect(() => {
-    startSwipeInterval();
-  }, []);
+    translateByIndex(1);
+    initScrollEvent();
+    setSwiperInterval();
+  });
 
-  const startSwipeInterval = () => {
+  const setSwiperInterval = () => {
+    const carouselElement = carouselRef.current;
+    if (!carouselElement) return;
+
+    swiperInterval = setInterval(() => {
+      swipe();
+    }, 3000);
+  };
+
+  const clearSwiperInterval = () => {
+    clearInterval(swiperInterval);
+  };
+
+  const translateByIndex = (i: number) => {
     const carouselElement = carouselRef.current;
     if (!carouselElement) return;
     const carouselElementWidth = carouselElement.clientWidth;
-    const swipeWidth = (carouselElementWidth * 85) / 100;
+    const hourseWidth = (carouselElementWidth * 85) / 100;
     const initWidth = (carouselElementWidth * 75) / 100;
-    carouselElement.scrollLeft = initWidth;
-    console.log(carouselElement.scrollLeft);
-    setInterval(() => {
-      const { scrollLeft } = carouselElement;
-      console.log(scrollLeft);
-      carouselElement.style.scrollBehavior = 'smooth';
-      carouselElement.scrollLeft = scrollLeft + swipeWidth;
-    }, 1000);
+    carouselElement.style.scrollBehavior = 'initial';
+    carouselElement.scrollLeft = initWidth + hourseWidth * i;
+  };
+
+  const swipe = () => {
+    const carouselElement = carouselRef.current;
+
+    if (!carouselElement) return;
+    const carouselElementWidth = carouselElement.clientWidth;
+    const hourseWidth = (carouselElementWidth * 85) / 100;
+    carouselElement.style.scrollBehavior = 'smooth';
+    carouselElement.scrollLeft += hourseWidth;
+  };
+  const createRenderData = (banners: Array<bannerInfo>) => {
+    const length = banners.length;
+    return [
+      banners[length - 2],
+      banners[length - 1],
+      ...banners,
+      banners[0],
+      banners[1],
+    ];
+  };
+
+  const getIndexByScrollLeft = () => {
+    const carouselElement = carouselRef.current;
+    if (!carouselElement) return;
+    const carouselElementWidth = carouselElement.clientWidth;
+    const hourseWidth = (carouselElementWidth * 85) / 100;
+    const initWidth = (carouselElementWidth * 75) / 100;
+    const scrollLeft = carouselElement.scrollLeft;
+    return Math.floor((scrollLeft - initWidth) / hourseWidth);
+  };
+
+  const initScrollEvent = () => {
+    const carouselElement = carouselRef.current;
+    if (!carouselElement) return;
+    let timer: any = null;
+    carouselElement.addEventListener('scroll', () => {
+      if (timer !== null) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(function () {
+        if (getIndexByScrollLeft() === banners.length + 1) translateByIndex(1);
+        else if (getIndexByScrollLeft() === 0) translateByIndex(banners.length);
+      }, 150);
+    });
+    carouselElement.addEventListener('touchstart', () => {
+      clearSwiperInterval();
+    });
+    carouselElement.addEventListener('touchend', () => {
+      setSwiperInterval();
+    });
   };
 
   const renderHorses = () => {
-    return bannerImg.map((info) => {
+    return createRenderData(banners).map((info, i) => {
       return (
-        <StyledHourse key={`horse_${info.id}`}>
-          <StyledThumb src={info.img_url} key={`img_${info.id}`} />
+        <StyledHourse key={`horse_${i}`}>
+          <StyledThumb src={info.img_url} key={`img_${i}`} />
         </StyledHourse>
       );
     });
   };
-
   return (
     <StyledWrapper>
       <StyledCarousel ref={carouselRef}>{renderHorses()}</StyledCarousel>
     </StyledWrapper>
   );
 }
+
+Carousel.defaultProps = {
+  banners: [
+    {
+      id: 0,
+      img_url:
+        'https://img-cf.kurly.com/shop/data/m/event/fa9d6731b3f39c03dc830c02c414cc52.jpg',
+    },
+    {
+      id: 1,
+      img_url:
+        'https://img-cf.kurly.com/shop/data/m/event/bc9b91586a619ffcfa8d7d5de83aae86.jpg',
+    },
+    {
+      id: 2,
+      img_url:
+        'https://img-cf.kurly.com/shop/data/m/event/758688e5267c724d33ebcab68de2599c.jpg',
+    },
+    {
+      id: 3,
+      img_url:
+        'https://img-cf.kurly.com/shop/data/m/event/fa9d6731b3f39c03dc830c02c414cc52.jpg',
+    },
+    {
+      id: 4,
+      img_url:
+        'https://img-cf.kurly.com/shop/data/m/event/bc9b91586a619ffcfa8d7d5de83aae86.jpg',
+    },
+  ],
+} as Partial<CarouselProps>;
