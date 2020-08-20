bdd28b89e3d999522f09add5e2eb29a3ee22ffe3
sooojungee <sooojungee@gmail.com>
2020-08-21 00:54:53 +0900

    pm2 아자아자

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 pwd>/home/ubuntu/location
 
 cd /home/ubuntu/build
-
 cd server
 cp ../../.env .env
 sudo rm -rf node_modules
 sudo yarn
 
-sudo fuser -k -n tcp 3000
-sudo yarn start
+# sudo fuser -k -n tcp 3000
+# sudo yarn start
+pm2 reload app-server 
