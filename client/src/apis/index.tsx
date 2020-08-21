8489ca7c7974b017fdc98ab9fa71d8c58684296e
myungwoo-Y <auddn6676@naver.com>
2020-08-21 13:42:40 +0900

    rebase

--- client/src/apis/index.tsx
+++ client/src/apis/index.tsx
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+// export default axios.create({
+//   baseURL: 'http://localhost:4000/api',
+// });
+
 export default axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'http://172.30.1.12:4000/api',
 });
