37753c66ede9dc3ecd2dbf7fcb172e9e133f0d9a
Myung <38170596+myungwoo-Y@users.noreply.github.com>
2020-08-22 12:33:28 +0900

    Update index.tsx

--- client/src/apis/index.tsx
+++ client/src/apis/index.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-// export default axios.create({
-//   baseURL: 'http://localhost:4000/api',
-// });
-
 export default axios.create({
-  baseURL: 'http://172.30.1.12:4000/api',
+  baseURL: 'http://localhost:4000/api',
 });
+
+// export default axios.create({
+//   baseURL: 'http://172.30.1.12:4000/api',
+// });
