ef096586c9899df1d781c8da28c961ba92b72cbf
yuda1124 <yuda1124@naver.com>
2020-08-24 15:13:17 +0900

    [#6] feat: 추가 - 카트페이지와 컴포넌트 연결

--- client/src/pages/Cart/Cart.styles.tsx
+++ client/src/pages/Cart/Cart.styles.tsx
@@ -1,3 +1,28 @@
 import styled from 'styled-components';
 
-export default {};
+export const StyledCartButton = styled.div`
+  width: calc(100% - 4vh);
+  border-radius: 3vh;
+  background: #dd4470;
+  bottom: 1vh;
+  height: 8vh;
+  color: white;
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 48px;
+  text-align: center;
+`;
+
+export const StyledCartWrapper = styled.div`
+  width: 100%;
+  padding-bottom: 9vh;
+`;
+
+export const StyledCartButtonWrapper = styled.div`
+  position: fixed;
+  width: 100%;
+  background-color: white;
+  height: 9vh;
+  bottom: 0;
+  padding: 0 2vh;
+`;
--- client/src/pages/Cart/Cart.tsx
+++ client/src/pages/Cart/Cart.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import CartProductList from '../../components/CartProductList';
+import {
+  StyledCartButton,
+  StyledCartWrapper,
+  StyledCartButtonWrapper,
+} from './Cart.styles';
+
 const Cart = () => {
   return (
-    <div>
+    <StyledCartWrapper>
       <h1>Cart</h1>
       <CartProductList />
-    </div>
+      <StyledCartButtonWrapper>
+        <StyledCartButton>담기</StyledCartButton>
+      </StyledCartButtonWrapper>
+    </StyledCartWrapper>
   );
 };
 
