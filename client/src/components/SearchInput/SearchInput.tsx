33a7d291ef7092ecbd80758334aa94ba321e0b65
myungwoo-Y <auddn6676@naver.com>
2020-08-25 04:54:41 +0900

    [#81] feat: 추가 - 검색창 스타일 수정

--- client/src/components/SearchInput/SearchInput.tsx
+++ client/src/components/SearchInput/SearchInput.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { faArrowLeft, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { StyledFormControl, StyledInput } from './SearchInput.styles';
 
 const SearchInput = () => {
-  return <div>Search Input</div>;
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  return (
+    <StyledFormControl fullWidth>
+      <StyledInput
+        id="standard-adornment-amount"
+        // onChange={handleChange('amount')}
+        startAdornment={
+          <InputAdornment position="start">
+            <FontAwesomeIcon icon={faArrowLeft} size="lg" />
+          </InputAdornment>
+        }
+        endAdornment={
+          <InputAdornment position="start">
+            <FontAwesomeIcon icon={faSearch} size="lg" />
+          </InputAdornment>
+        }
+      />
+    </StyledFormControl>
+  );
 };
 
 export default SearchInput;
