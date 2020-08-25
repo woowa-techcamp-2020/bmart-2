1484143ee9e25cd471ac9fd34a7f1c4193f7dbee
yuda1124 <yuda1124@naver.com>
2020-08-26 03:36:46 +0900

    [#88] feat: 추가 - 찜 API
    - 찜 추가 API
    - 찜 delete API
    - 찜 Get API

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,5 +1,6 @@
 import CategoryService from './categoryService';
 import ProcutService from './productService';
 import CartService from './cartService';
+import DibService from './dibService';
 
-export { CategoryService, ProcutService, CartService };
+export { CategoryService, ProcutService, CartService, DibService };
