580e6dabfc197ecd44660c32936c9148c698d463
myungwoo-Y <auddn6676@naver.com>
2020-08-26 21:38:52 +0900

    [#43] fix:수정 - Main 리팩토링 겹치는 코드 리팩토링

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import SaleNow from '../../components/SaleNow';
 import Pull from '../../components/Pull';
 import Carousel from '../../components/Carousel';
@@ -8,18 +8,19 @@ import MainProductList from '../../components/MainProductList';
 import { ICategory } from '../../../../types/modelTypes';
 import apis from '../../apis';
 
-let lastTouch: { x: number; y: number } = {
-  x: 0,
-  y: 0,
-};
-let startTouch: { x: number; y: number } = {
-  x: 0,
-  y: 0,
-};
+let lastTouchY = 0;
+let startTouchY = 0;
+const minBoxSize = 100;
+
+const defaultTransitionTime = 0;
+const finishTransitionTime = 200;
+let isPulling = false;
+let timeoutId: number | undefined;
+let isFinishing = false;
+
 const Main = () => {
   const [boxHeight, setBoxHeight] = useState(0);
-  const [isPulling, setIsPulling] = useState(false);
-  const [transitionTime, setTransitionTime] = useState(0);
+  const transitionContainerRef = useRef<HTMLDivElement>(null);
   const [productsInCategories, setProductsInCategories] = useState<ICategory[]>(
     []
   );
@@ -31,80 +32,103 @@ const Main = () => {
     };
     getProductByCategory();
   }, []);
-  const defaultTransitionTime = 0;
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  const setTransitionTime = (time: number) => {
+    if (transitionContainerRef.current) {
+      if (transitionContainerRef.current.style) {
+        transitionContainerRef.current.style.transition = `transform ${time}ms linear`;
+      }
+    }
+  };
   const onTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
+    if (isPulling) return;
     setTransitionTime(defaultTransitionTime);
     if (event.targetTouches && !isPulling) {
       const touch = event.targetTouches[0];
-      lastTouch = { x: touch.clientX, y: touch.clientY };
-      startTouch = { x: touch.clientX, y: touch.clientY };
-      setIsPulling(true);
+      lastTouchY = touch.clientY;
+      startTouchY = touch.clientY;
+      isPulling = true;
     }
+  };
+
+  const setBoxHeightByDiff = (touch: React.Touch) => {
+    const defaultPadding = 80;
+    const heightDiffer =
+      touch.clientY -
+      startTouchY +
+      (isFinishing ? minBoxSize + defaultPadding : 0);
+    console.log(heightDiffer);
+    const newHeight = Math.round(heightDiffer / 2);
+    if (newHeight === boxHeight) return;
+    setBoxHeight(newHeight);
   };
 
   const isScrollInTop = () => {
     return window.scrollY === 0;
   };
 
-  const onTouchEnd = () => {
-    const minBoxSize = 100;
-    if (boxHeight > minBoxSize) {
-      setBoxHeight(minBoxSize);
-      setTimeout(() => {
-        setBoxHeight(0);
-      }, 2 * 1000);
-    } else {
-      setBoxHeight(0);
-    }
-
-    setIsPulling(false);
-    setTransitionTime(200);
-  };
-
   const onTouchMove = (event: React.TouchEvent<HTMLDivElement>) => {
     if (!isPulling) return;
     const { touches } = event;
     const touch = touches[0];
-    if (touch.clientY - lastTouch.y > 0 && isScrollInTop()) {
-      const heightDiffer = touch.clientY - startTouch.y;
-      setBoxHeight(Math.round(heightDiffer / 2));
-      lastTouch = { x: touch.clientX, y: touch.clientY };
-    } else if (touch.clientY - lastTouch.y < 0) {
-      const heightDiffer = touch.clientY - startTouch.y;
-      const newHeight = Math.round(heightDiffer / 2);
-      if (newHeight === boxHeight) return;
-      setBoxHeight(newHeight);
+    if (touch.clientY - lastTouchY > 0 && isScrollInTop()) {
+      setBoxHeightByDiff(touch);
+      lastTouchY = touch.clientY;
+    } else if (touch.clientY - lastTouchY < 0) {
+      setBoxHeightByDiff(touch);
     }
   };
 
-  const transformOption = () => {
-    if (boxHeight <= 0) return `translate(0px, 0px)`;
-    return `translate(0px, ${boxHeight}px)`;
+  const setBoxHeightToZero = () => {
+    const second = 2;
+    setTransitionTime(finishTransitionTime);
+    setBoxHeight(minBoxSize);
+    isFinishing = true;
+    // finish동작은 동작 중 1번만 일어나게
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      setBoxHeight(0);
+      isFinishing = false;
+    }, second * 1000);
+  };
+
+  const onTouchEnd = () => {
+    if (boxHeight > minBoxSize) {
+      setBoxHeightToZero();
+    } else {
+      setBoxHeight(0);
+    }
+    isPulling = false;
   };
 
   const getMainProductList = useMemo(() => {
     return <MainProductList productsInCategories={productsInCategories} />;
   }, [productsInCategories.length]);
 
+  const transformOption = () => {
+    if (boxHeight <= 0) return `translate(0px, 0px)`;
+    return `translate(0px, ${boxHeight}px)`;
+  };
+
   return (
     <StyledMainWrap
-      transitionTime={transitionTime}
+      transitionTime={defaultTransitionTime}
       onTouchStart={onTouchStart}
       onTouchMove={onTouchMove}
       onTouchEnd={onTouchEnd}
     >
       <Pull boxHeight={boxHeight} isPulling={isPulling} />
-      <div style={{ transform: transformOption() }}>
+      <div
+        style={{ transform: transformOption() }}
+        ref={transitionContainerRef}
+      >
         <CategoryIcons />
-        {/* <SearchInput /> */}
         <Carousel />
         <SaleNow />
-        {/* {boxHeight <= 0 ? <MainProductList /> : <></>} */}
         {getMainProductList}
       </div>
     </StyledMainWrap>
