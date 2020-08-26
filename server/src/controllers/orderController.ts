bb755635ba6a9c2274a8a9eef942a99e2ff9db11
yuda1124 <yuda1124@naver.com>
2020-08-27 01:21:46 +0900

    [#89] feat: 추가 - userId로 order 가져오는 API

--- server/src/controllers/orderController.ts
+++ server/src/controllers/orderController.ts
@@ -1,3 +1,7 @@
+/* eslint-disable no-shadow */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Request, Response, NextFunction } from 'express';
 import Order from '../models/Order';
 import { OrderService } from '../services';
@@ -19,9 +23,23 @@ const create = async (
 ): Promise<void> => {
   const { userId, products } = req.body as IOrder;
   const order = await Order.create({ userId });
-  console.log(order);
   await OrderService.createOrderProductRelation(order.id, products);
   res.status(200).send({ success: true });
 };
 
-export default { create };
+const findByUserId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const { params } = req;
+  const userId = parseInt(params.userId, 10);
+  const result = await OrderService.findByUserId(userId);
+  const orders = result.map((row: any) => {
+    const { userId, orderId, lastUpdated, count, ...product } = row;
+    return { userId, orderId, lastUpdated, count, product };
+  });
+  res.status(200).send({ success: true, orders });
+};
+
+export default { create, findByUserId };
--- server/src/services/orderService.ts
+++ server/src/services/orderService.ts
@@ -1,4 +1,6 @@
+import { QueryTypes } from 'sequelize';
 import OrderProductRelation from '../models/OrderProductRelation';
+import { sequelize } from '../models';
 
 interface IOrderProduct {
   id: number;
@@ -20,4 +22,15 @@ const createOrderProductRelation = async (
   );
 };
 
-export default { createOrderProductRelation };
+const FIND_BY_USER_ID =
+  'SELECT REL.orderId, REL.lastUpdated, REL.userId, REL.count, P.* from product P, (select R.productId, R.count, O.orderId, O.lastUpdated, O.userId from orderProductRelation R, (select id as orderId, updatedAt as lastUpdated, userId from `order` where userId = :userId) O where O.orderId = R.orderId) REL where REL.productId = P.id;';
+
+const findByUserId = async (userId: number): Promise<any> => {
+  const result = await sequelize.query(FIND_BY_USER_ID, {
+    replacements: { userId },
+    type: QueryTypes.SELECT,
+  });
+  return result;
+};
+
+export default { createOrderProductRelation, findByUserId };
