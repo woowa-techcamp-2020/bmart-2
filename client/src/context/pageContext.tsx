83fef6cd33a4fa74f5162a22071f8b4419be323c
yuda1124 <yuda1124@naver.com>
2020-08-27 23:05:08 +0900

    [#113] feat: 구현 - pageContext
    - 전 영역에서 Notification을 할 수 있도록 Context API 사용
    
    related issue: #issue
    related commit: commit_hash

---
+++ client/src/context/pageContext.tsx
@@ -0,0 +1,54 @@
+import React, { createContext, Dispatch, useContext, useReducer } from 'react';
+
+export interface IPageInContext {
+  onNotification: boolean;
+  message: string;
+}
+
+export type TPageState = IPageInContext;
+
+const PageStateContext = createContext<TPageState | undefined>(undefined);
+
+type TAction = { type: 'NOTI_OPEN'; message: string };
+
+type TPageDispatch = Dispatch<TAction>;
+const PageDispatchContext = createContext<TPageDispatch | undefined>(undefined);
+
+function pageReducer(state: TPageState, action: TAction): TPageState {
+  switch (action.type) {
+    case 'NOTI_OPEN':
+      return { ...state, message: action.message };
+    default:
+      throw new Error('Unhandled action');
+  }
+}
+
+export function PageContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [page, dispatch] = useReducer(pageReducer, {
+    onNotification: false,
+    message: '',
+  });
+
+  return (
+    <PageDispatchContext.Provider value={dispatch}>
+      <PageStateContext.Provider value={page}>
+        {children}
+      </PageStateContext.Provider>
+    </PageDispatchContext.Provider>
+  );
+}
+
+export function usePageState() {
+  const state = useContext(PageStateContext);
+  if (!state) throw new Error('pageProvider not found');
+  return state;
+}
+
+export function usePageDispatch() {
+  const dispatch = useContext(PageDispatchContext);
+  return dispatch;
+}
