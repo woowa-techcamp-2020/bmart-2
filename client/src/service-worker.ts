329b07b2348ee7fc739034950809489497190307
myungwoo-Y <auddn6676@naver.com>
2020-08-28 06:25:11 +0900

    [#127]feat: 추가 - injectmanifest 추가

--- client/src/service-worker.ts
+++ client/src/service-worker.ts
@@ -1,11 +1,59 @@
-// @ts-ignore
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js'
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { precacheAndRoute } from 'workbox-precaching';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+
+registerRoute(
+  // Cache style resources, i.e. CSS files.
+  ({ request }) => request.destination === 'style',
+  // Use cache but update in the background.
+  new StaleWhileRevalidate({
+    // Use a custom cache name.
+    cacheName: 'css-cache',
+  })
 );
 
-// @ts-ignore
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
+registerRoute(
+  new RegExp('https://localhost:4000/(.*)'),
+  new CacheFirst({
+    cacheName: 'tvmaze-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 30,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  /\.(?:png|jpg|jpeg|svg)$/,
+  new CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 100,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+registerRoute(
+  /\.(?:css|js)$/,
+  new StaleWhileRevalidate({
+    cacheName: 'scripts',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 100,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+precacheAndRoute(self.__WB_MANIFEST);
