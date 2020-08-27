38d40ab036592f7f746789a84f09ff2f99142eba
myungwoo-Y <auddn6676@naver.com>
2020-08-27 20:10:24 +0900

    [#81] feat: 추가 - SearchHistory db에서 데이터 가져와 컴포넌트생성

--- client/src/components/SearchHistory/SearchHistory.tsx
+++ client/src/components/SearchHistory/SearchHistory.tsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { List, ListItem, ListItemText } from '@material-ui/core';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { getHistories } from '../../apis/searchHistory';
 import {
   StyledDeleteIcon,
   StyledListHeader,
   StyledSearchHistoryWrap,
   StyledListItem,
 } from './SearchHistory.styles';
+import { ISearchHistory } from '../../../../types/modelTypes';
 
 const datas = ['과자', '두유', '우유', '라면', '야채'];
 
 const SearchHistory = () => {
+  const [histories, setHistories] = useState<ISearchHistory[]>([]);
+  useEffect(() => {
+    const getSearchHistories = async () => {
+      const newHistories = await getHistories();
+      setHistories(newHistories);
+    };
+    getSearchHistories();
+  }, []);
   const historyList = () =>
-    datas.map((text, idx) => (
-      <StyledListItem key={idx} button>
-        <ListItemText primary={text} />
+    histories.map((searchHistory) => (
+      <StyledListItem key={searchHistory.id} button>
+        <ListItemText primary={searchHistory.keyword} />
         <StyledDeleteIcon>
           <FontAwesomeIcon icon={faTimes} size="lg" />
         </StyledDeleteIcon>
