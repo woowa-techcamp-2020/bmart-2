fdbd506fb4dfa1fda4a0160da0a64ebd85cb195d
yuda1124 <yuda1124@naver.com>
2020-08-27 18:33:11 +0900

    [#107] feat: 추가 - date string을 formatting 해주는 util 함수

--- client/src/util/common.tsx
+++ client/src/util/common.tsx
@@ -2,4 +2,10 @@ function numberToString(x: number) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
-export { numberToString };
+function dateToString(dateStr: string) {
+  const date = new Date(dateStr);
+  return `${date.getMonth() + 1}월 ${date.getDate()}일 ${
+    date.getHours() < 10 ? '0' + date.getHours() : date.getHours()
+  }시 ${date.getMinutes()}분`;
+}
+export { numberToString, dateToString };
