aea59a6a68a40c6cbf4cd7164e71b6beeb26cae6
yuda1124 <yuda1124@naver.com>
2020-08-27 23:55:26 +0900

    [#113] feat: 적용 - context api
    - context api 적용
    - 찜 버튼 클릭 시 notification 뜨도록 수정
    
    related issue: #issue
    related commit: commit_hash

--- client/src/components/Notification/Notification.styles.tsx
+++ client/src/components/Notification/Notification.styles.tsx
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 import MuiAlert from '@material-ui/lab/Alert';
 
 export const StyledNotification = styled(MuiAlert)`
-  background-color: #f50057 !important;
+  background-color: #000 !important;
+  opacity: 0.7 !important;
   .MuiAlert-message,
   .MuiSvgIcon-root {
     color: #fff !important;
--- client/src/components/Notification/Notification.tsx
+++ client/src/components/Notification/Notification.tsx
@@ -2,33 +2,36 @@ import React from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import { AlertProps } from '@material-ui/lab/Alert';
 import { StyledNotification } from './Notification.styles';
+import {
+  usePageState,
+  IPageInContext,
+  usePageDispatch,
+  closeNotification,
+} from '../../context/pageContext';
 
 function Alert(props: AlertProps) {
   return <StyledNotification elevation={6} variant="filled" {...props} />;
 }
 
 export default function Notification() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleClick = () => {
-    setOpen(true);
-  };
+  const page: IPageInContext = usePageState();
+  const dispatch = usePageDispatch();
 
   const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
     if (reason === 'clickaway') {
       return;
     }
-
-    setOpen(false);
+    closeNotification(dispatch);
   };
   return (
-    <div>
-      <button onClick={handleClick}>Open success snackbar</button>
-      <Snackbar open={open} autoHideDuration={1000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="info">
-          This is a success message!
-        </Alert>
-      </Snackbar>
-    </div>
+    <Snackbar
+      open={page.onNotification}
+      autoHideDuration={10000}
+      onClose={handleClose}
+    >
+      <Alert onClose={handleClose} severity="info">
+        {page.message}
+      </Alert>
+    </Snackbar>
   );
 }
