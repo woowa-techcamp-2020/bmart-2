e703eeb1f8689cd5317e4ac4c37d6e623f9a88ea
yuda1124 <yuda1124@naver.com>
2020-08-27 15:46:53 +0900

    [#107] feat: 추가 - order 관련 API 호출 함수

---
+++ client/src/apis/order.tsx
@@ -0,0 +1,13 @@
+import api from './util';
+
+export const getOrders = async () => {
+  const response = await api.get(`/order`);
+  return response;
+};
+
+export const createOrder = async (body: {
+  products: { id: number; count: number }[];
+}) => {
+  const response = await api.post('order', body);
+  return response.data;
+};
